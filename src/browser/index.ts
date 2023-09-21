bbf8b994b8ce5aa426f4db4c46b90c2be8c8581b
Wii Yang <wii_yang@163.com>
2023-09-21 17:24:43 +0800

    fix: 处理控制台警告信息

--- src/browser/index.ts
+++ src/browser/index.ts
@@ -1,4 +1,4 @@
-import { ServiceBuilder } from 'selenium-webdriver/chrome'
+import { ServiceBuilder, Options } from 'selenium-webdriver/chrome'
 import { Builder, WebDriver } from 'selenium-webdriver'
 import { config } from '../utils'
 import User from '../user'
@@ -9,8 +9,20 @@ import User from '../user'
 export async function createBrowser(): WebDriver {
   // 获取 chromedriver
   const service: ServiceBuilder = new ServiceBuilder(config.chromedriverFileUrl)
+
+  // 浏览器配置
+  const options: Options = new Options()
+  options.addArguments('--ignore-certificate-errors')
+  options.addArguments('--ignore-ssl-errors')
+
+  const builder: Builder = new Builder()
+  builder.forBrowser('chrome')
+  builder.setChromeService(service)
+  builder.setChromeOptions(options)
+
   // 创建浏览器实例
-  const driver: WebDriver = new Builder().forBrowser('chrome').setChromeService(service).build()
+  const driver: WebDriver = builder.build()
+
   // 浏览器窗口最大化
   await driver.manage().window().maximize()
 
