a040ac153cd9856dd5757473b36071e9d91fa051
Wii-Yang <wii_yang@163.com>
2025-12-04 11:35:05 +0800

    fix: 调整 chromedriver 读取路径

--- src/utils/Config.ts
+++ src/utils/Config.ts
@@ -1,7 +1,7 @@
 import * as path from 'path'
 
 // chromedriver 文件地址
-const chromedriverFileUrl: string = path.join(process.cwd(), 'chromedriver.exe')
+const chromedriverFileUrl: string = path.join(process.cwd(), 'chromedriver')
 
 // 用户信息文件地址
 const userDataBase: string = path.join(process.cwd(), 'user.json')
