58a18d5834b513a1d42c38621adf02c451b288ce
Wii-Yang <wii_yang@163.com>
2025-12-04 11:48:15 +0800

    fix: 处理没有弹窗时导致报错的问题

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -18,20 +18,24 @@ function getStrLength(str: string): number {
 async function waitAndCloseDialog(browser: WebDriver): Promise<void> {
   await browser.sleep(1000 * 2)
 
-  // 判断是否有弹窗
-  const dialog: WebElement = await browser.findElement(By.className('el-dialog__wrapper dialog_'))
-  const style: string = await dialog.getAttribute('style')
-  if (style.search('display: none;') < 0) {
-    // 等待弹窗加载完成
-    await browser.wait(
-      until.elementLocated(By.className('el-button el-button--small el-button--default'))
-    )
-    // 当弹窗打开时关闭弹窗
-    const dialogFooter: WebElement = await dialog.findElement(By.className('el-dialog__footer'))
-    const closeButton: WebElement = await dialogFooter.findElement(
-      By.className('el-button el-button--small el-button--default')
-    )
-    await closeButton.click()
+  try {
+    // 判断是否有弹窗
+    const dialog: WebElement = await browser.findElement(By.className('el-dialog__wrapper dialog_'))
+    const style: string = await dialog.getAttribute('style')
+    if (style.search('display: none;') < 0) {
+      // 等待弹窗加载完成
+      await browser.wait(
+        until.elementLocated(By.className('el-button el-button--small el-button--default'))
+      )
+      // 当弹窗打开时关闭弹窗
+      const dialogFooter: WebElement = await dialog.findElement(By.className('el-dialog__footer'))
+      const closeButton: WebElement = await dialogFooter.findElement(
+        By.className('el-button el-button--small el-button--default')
+      )
+      await closeButton.click()
+    }
+  } catch (e) {
+    //
   }
 }
 
